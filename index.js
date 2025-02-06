7187b9973149d8079b0bd89ccf14aea160b86879
Gísli Leifsson <gisli.leifsson@netapp.com>
2025-02-06 13:24:27 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const { execFile } = require('child_process');
 const path = require('path');
 
 module.exports = async function (context, req) {
-    const goBinary = path.join(__dirname, 'gin');
-    execFile(goBinary, (error, stdout, stderr) => {
+    const goBinary = path.join(__dirname, 'myGoApp');
+    const args = [];
+
+    // Forward the request path and query parameters to the Go binary
+    if (req.query) {
+        for (const key in req.query) {
+            args.push(`--${key}=${req.query[key]}`);
+        }
+    }
+
+    execFile(goBinary, args, (error, stdout, stderr) => {
         if (error) {
             context.log.error(`Error: ${stderr}`);
             context.res = {
